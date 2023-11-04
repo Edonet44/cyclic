bf35259f312552007ced195b9e7356c3d0590401
Edonet44 <eddieting33@gmail.com>
2023-11-04 15:48:46 +0100

    test node

--- server.js
+++ server.js
@@ -20,29 +20,29 @@ const plantsData = [
   // Aggiungi altri dati delle piante
 ];
 
-const https = require('https');
+//const https = require('https');
 const express = require('express')
 const app = express();
 
 //abilitare i Cors
-https.createServer(options, (req, res) => {
-  res.writeHead(200, {
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
-  });
-  res.end(JSON.stringify({
-     version: '1.0',
-     endpoint: req.url,
-     method: req.method,
-     headers: req.headers 
-  }, null, 2));
-}).listen(PORT);
+// https.createServer(options, (req, res) => {
+//   res.writeHead(200, {
+//       'Content-Type': 'application/json',
+//       'Access-Control-Allow-Origin': '*',
+//       'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+//   });
+//   res.end(JSON.stringify({
+//      version: '1.0',
+//      endpoint: req.url,
+//      method: req.method,
+//      headers: req.headers 
+//   }, null, 2));
+// }).listen(PORT);
 
-// app.all('/', (req, res) => {
-//     console.log("Just got a request!")
-//     res.send('Yo!')
-// });
+app.all('/', (req, res) => {
+    console.log("Richiesta in corso")
+    res.send('Server ok')
+});
 // Gestisci la richiesta per ottenere tutti i dati delle piante
 app.get('/plants', (req, res) => {
   res.json(plantsData);
