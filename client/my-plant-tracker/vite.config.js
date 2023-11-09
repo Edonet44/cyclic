f2bcd2cd8bfaa0c8e3131e75aaf41ecc7439c3f9
Edonet44 <eddieting33@gmail.com>
2023-11-09 22:11:22 +0100

    axios upd plant detail4

--- client/my-plant-tracker/vite.config.js
+++ client/my-plant-tracker/vite.config.js
@@ -27,8 +27,9 @@ export default defineConfig({
        '/plants/:id': { // Esempio di percorso con un parametro ID
       target: 'https://yellow-vulture-suit.cyclic.app', // dominio server
       changeOrigin: true,
-       rewrite: (path) => path.replace(/^\/plants\/id\//, '/'), // Assicurati che il parametro ID venga incluso
-      headers: {
+      // rewrite: (path) => path.replace(/^\/plants\/id\//, '/'), // Assicurati che il parametro ID venga incluso
+      rewrite: (path) => path.replace(/^\/plants\//, '/'),
+         headers: {
         'Access-Control-Allow-Origin': 'https://myplanttracker-2e0a9.web.app', // dominio client
         'Access-Control-Allow-Credentials': 'true',
       },
